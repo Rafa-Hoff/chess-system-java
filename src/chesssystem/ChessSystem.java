5b073396d9c0f029bd3e38170d418ac7aa19c0a4
Rafa-Hoff <rafahoff32@gmail.com>
2022-07-28 12:56:10 -0400

    Starting to implement Board and Piece

--- src/chesssystem/ChessSystem.java
+++ src/chesssystem/ChessSystem.java
@@ -4,6 +4,7 @@
  */
 package chesssystem;
 
+import boardgame.Board;
 import boardgame.Position;
 
 /**
@@ -17,8 +18,7 @@ public class ChessSystem {
      */
     public static void main(String[] args) {
         
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
     }
     
 }
